b28426c72c62752faf27a3b4215875dce37ffcdf
Sergey <gck.sergey@gmail.com>
2018-09-19 21:21:29 +0300

    gotovo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let arr = [];
+	bracketsConfig.forEach(i=>{
+		arr.push(i.join(''));
+	});
+	let count;
+	do{count = 0;
+		for(let i = 0; i<arr.length; i++){
+			if(str.indexOf(arr[i])>=0){
+				count++; str = str.replace(arr[i],'')
+			}
+		}
+	}while(count!=0);	
+	return str.length == 0;
 }
